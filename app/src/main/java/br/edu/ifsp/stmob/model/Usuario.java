6ce786a3f03c3beecefac70eedcd0a0d014ea832
FilipeNavas <filipenavas@hotmail.com>
2015-10-26 16:32:54 -0200

    Adicionado classse modelo Usuario

---
+++ app/src/main/java/br/edu/ifsp/stmob/model/Usuario.java
@@ -0,0 +1,72 @@
+package br.edu.ifsp.stmob.model;
+
+/**
+ * Created by Filipe on 2015-10-26.
+ */
+public class Usuario {
+
+    int usuCod;
+    String usuNome;
+    String usuEmail;
+    String usuSenha;
+    String usuTipo;
+    String usuTelefone;
+
+    public void cadastrarUsuario(Usuario usuario){}
+
+    public void logar(String usuEmail, String usuSenha){}
+
+    public void alterarInf(String usuNome, String usuTelefone){}
+
+    public void recuperarSenha(String usuEmail){}
+
+    public int getUsuCod() {
+        return usuCod;
+    }
+
+    public void setUsuCod(int usuCod) {
+        this.usuCod = usuCod;
+    }
+
+    public String getUsuNome() {
+        return usuNome;
+    }
+
+    public void setUsuNome(String usuNome) {
+        this.usuNome = usuNome;
+    }
+
+    public String getUsuEmail() {
+        return usuEmail;
+    }
+
+    public void setUsuEmail(String usuEmail) {
+        this.usuEmail = usuEmail;
+    }
+
+    public String getUsuSenha() {
+        return usuSenha;
+    }
+
+    public void setUsuSenha(String usuSenha) {
+        this.usuSenha = usuSenha;
+    }
+
+    public String getUsuTipo() {
+        return usuTipo;
+    }
+
+    public void setUsuTipo(String usuTipo) {
+        this.usuTipo = usuTipo;
+    }
+
+    public String getUsuTelefone() {
+        return usuTelefone;
+    }
+
+    public void setUsuTelefone(String usuTelefone) {
+        this.usuTelefone = usuTelefone;
+    }
+}
+
+
