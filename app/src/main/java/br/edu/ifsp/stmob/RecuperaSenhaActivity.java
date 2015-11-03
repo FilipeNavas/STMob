81548adb93d83d0b56d2c0c4aa2f21545257e15d
Leticia <leticiahisaeyanaguya@gmail.com>
2015-11-02 22:22:49 -0200

    Inserção de activity de Busca de Aviso Extraordinário e Recuperar Senha.

---
+++ app/src/main/java/br/edu/ifsp/stmob/RecuperaSenhaActivity.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.stmob;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecuperaSenhaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recupera_senha);
+    }
+}
