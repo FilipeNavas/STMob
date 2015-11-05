01c07f9eeb45eefa18e7d1172242b9520036b14a
Leticia <leticiahisaeyanaguya@gmail.com>
2015-11-04 22:12:14 -0200

    Inserção de activity de Cadastro de Aviso Extraodinário.

---
+++ app/src/main/java/br/edu/ifsp/stmob/CadastraAvisoExtraordinarioActivity.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.stmob;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastraAvisoExtraordinarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastra_aviso_extraordinario);
+    }
+}
